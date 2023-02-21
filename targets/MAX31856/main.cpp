47254021204c339591201c47ffd27fad33bdf300
Ben Cerrone <bwc2184@rit.edu>
2023-02-20 21:10:19 -0500

    updated how the temp is read

--- targets/MAX31856/main.cpp
+++ targets/MAX31856/main.cpp
@@ -11,7 +11,7 @@ constexpr uint8_t deviceCount = 1;
 IO::GPIO* devices[deviceCount];
 
 // need to verify spi settings:
-// https://www.analog.com/media/en/technical-documentation/data-sheets/MAX31856.pdf
+// https://www.analog.com/media/en/technical-documentation/data-sheets/MAX31855.pdf
 
 int main() {
     IO::init();
@@ -29,10 +29,18 @@ int main() {
     //int32_t temp = MAX.readTemp();
     const int length = 4;
     uint8_t bytes[length];
+    uint16_t temp = 0;
     uart.printf("read start:\r\n");
 
-    for (int i=0; i<=15; i++) {
-        spi.readReg(0, i, bytes, length);
-        uart.printf("reg[%d] values: %x, %x\r\n", i, bytes[0], bytes[1]);
+    while (true) {
+        spi.startTransmission(0);
+        spi.read(bytes, length);
+        spi.endTransmission(0);
+        temp = (((uint16_t)bytes[0]) << 8) | bytes[1];
+        temp = temp >> 2; // lose last 2 bits as they aren't in the 14 bit temp
+        temp = temp >> 2; // move over 2 more for decimal
+        uart.printf("temp: %d\r\n", temp);
+        break;
+        EVT::core::time::wait(3000);
     }
 }
