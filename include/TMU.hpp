4f4810cc008c28e8341db89811508229ca9d046b
GitHub Build <N/A>
2023-02-24 01:16:39 +0000

    Applied Formatting Changes During GitHub Build

--- include/TMU.hpp
+++ include/TMU.hpp
@@ -104,22 +104,26 @@ private:
             .Type = nullptr,
             .Data = (uintptr_t) 5,
         },
-        {//180h+Node-ID
+        {
+            //180h+Node-ID
             .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0) + NODE_ID,
         },
-        {//timer triggered
+        {
+            //timer triggered
             .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 0xFE,
         },
-        {//no inhibit time
+        {
+            //no inhibit time
             .Key = CO_KEY(0x1800, 3, CO_UNSIGNED16 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 0,
         },
-        {//send every 2 seconds
+        {
+            //send every 2 seconds
             .Key = CO_KEY(0x1800, 5, CO_UNSIGNED16 | CO_OBJ_D__R_),
             .Type = CO_TEVENT,
             .Data = (uintptr_t) 2000,
