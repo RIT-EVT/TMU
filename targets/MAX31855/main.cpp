f8afb6621f926fb2234e207ca10316685a16e3d4
Aaron Chan <aarchan108@gmail.com>
2023-04-03 19:11:13 -0400

    Added 4 different devices

--- targets/MAX31855/main.cpp
+++ targets/MAX31855/main.cpp
@@ -7,7 +7,7 @@
 namespace IO = EVT::core::IO;
 
 constexpr uint32_t SPI_SPEED = SPI_SPEED_1MHZ;
-constexpr uint8_t deviceCount = 1;
+constexpr uint8_t deviceCount = 4;
 IO::GPIO* devices[deviceCount];
 
 int main() {
@@ -16,8 +16,18 @@ int main() {
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
     // Setup SPI
-    devices[0] = &IO::getGPIO<IO::Pin::SPI_CS>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[0] = &IO::getGPIO<IO::Pin::PB_4>(EVT::core::IO::GPIO::Direction::OUTPUT);
     devices[0]->writePin(IO::GPIO::State::HIGH);
+
+    devices[1] = &IO::getGPIO<IO::Pin::PB_5>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[1]->writePin(IO::GPIO::State::HIGH);
+
+    devices[2] = &IO::getGPIO<IO::Pin::PB_6>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[2]->writePin(IO::GPIO::State::HIGH);
+
+    devices[3] = &IO::getGPIO<IO::Pin::PB_7>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[3]->writePin(IO::GPIO::State::HIGH);
+
     IO::SPI& spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI, IO::Pin::SPI_MISO>(devices, deviceCount);
     spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
 
